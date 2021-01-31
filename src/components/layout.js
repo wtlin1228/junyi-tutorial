8d99b903d25bca65f6a2b9b4b80771456c020d55
wtlin <wtlin1228@gmail.com>
2021-01-31 13:00:45 +0800

    feat: gatsby can load /docs and understand mdx

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,12 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Header from "./header"
 import "./layout.css"
+
+import { buildMenuData } from "../../utils/build-menu-data"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -20,29 +22,61 @@ const Layout = ({ children }) => {
           title
         }
       }
+      allDirectory(
+        filter: { relativeDirectory: { regex: "/[0-9]{2}-[a-zA-Z]*/i" } }
+      ) {
+        nodes {
+          name
+          relativePath
+        }
+      }
     }
   `)
 
+  const menuData = buildMenuData(data.allDirectory.nodes)
+
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <Header siteTitle={data.site.siteMetadata.title || `Title`} />
       <div
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          display: "flex",
         }}
       >
-        <main>{children}</main>
-        <footer
+        <div
           style={{
-            marginTop: `2rem`,
+            width: 300,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+          {menuData.map(({ title, items }) => (
+            <div key={`${title}`}>
+              <div>{title}</div>
+              {items.map(({ title: subTitle, path }) => (
+                <div key={path}>
+                  <Link to={path}>{subTitle}</Link>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0 1.0875rem 1.45rem`,
+          }}
+        >
+          <main>{children}</main>
+          <footer
+            style={{
+              marginTop: `2rem`,
+            }}
+          >
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.com">Gatsby</a>
+          </footer>
+        </div>
       </div>
     </>
   )
